dd6fa6c01f9c947ef69307337ca7f3c27e402ccc
Edward White <ewhite91186@gmail.com>
2018-04-03 18:40:00 -0500

    insert message to db

--- database/db.js
+++ database/db.js
@@ -20,4 +20,22 @@ function insertUser(name, email) {
 	user.save();
 }
 
+const messagesSchema = mongoose.Schema({
+	user: { type: String, required: true },
+	message: { type: String, required: true }
+});
+
+const Message = mongoose.model('Message', messagesSchema);
+
+function insertMessage(name, message) {
+	let msg = new Message({
+		user: name,
+		message: message
+	})
+	msg.save();
+}
+
 exports.insertUser = insertUser;
+exports.insertMessage = insertMessage;
+exports.user = User;
+exports.message = Message;
